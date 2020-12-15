b4052058859984fc4f67fdd99d27af585226e352
nadpescador <santiago.calvi@davinci.edu.ar>
2020-12-14 23:29:05 -0300

    añadi cambios, puse una lista de fotos de lorem photum con scroll y push to refresh

--- lib/src/models/images_model.dart
+++ lib/src/models/images_model.dart
@@ -1,25 +1,11 @@
 
-class ImagesModel {
-  List<Images> items = new List();
-
-  ImagesModel();
-
-  ImagesModel.fromJsonList(List<dynamic> jsonList){
-    if (jsonList == null) return;
-
-    for(var item in jsonList){
-      final imagen = new Images.fromJsonMap(item);
-            items.add(imagen);
-          }
-        } 
-      }
-      
-      class Images {
+class Images {
         String id;
         String author;
         int width;
         int height;
         String url;
+        String downloadUrl;
       
         Images({
           this.id,
@@ -27,15 +13,9 @@ class ImagesModel {
           this.width,
           this.height,
           this.url,
+          this.downloadUrl,
         });
-      
-        Images.fromJsonMap(Map<String,dynamic> json){
-          id      = json["id"];    
-          author  = json["author"];
-          url     = json["url"];
-          width   = json["width"];
-          height  = json["height"];
-        }
 
 
-  }
+}
+    
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
 import 'package:ejercicio_urbetrack/src/blocs/login_bloc.dart';
 import 'package:ejercicio_urbetrack/src/blocs/provider.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 
 
 void main() {
@@ -16,21 +20,48 @@ class HomePage extends StatefulWidget {
 
 class HomePageState extends State<HomePage> {
 
+ScrollController _scrollController = new ScrollController();
+
+  List<int> _numeros = new List();
+  int _ultimoItem=0;
+  bool _isLoading = false;
+
+
+
+  @override
+  void initState() {
+    
+    super.initState();
+    _agregar10();
+
+    _scrollController.addListener(() {
+      if(_scrollController.position.pixels == _scrollController.position.maxScrollExtent){
+        //_agregar10();
+        fetchData();
+      }
+
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
+
     final _bloc = Provider.of(context);
 
     return Scaffold(
       appBar: AppBar(backgroundColor:Colors.deepPurpleAccent),
       drawer: _drawer(_bloc),
-      body: Text("Hola MUndo"),
+      body: Center(
+        child: Container(
+          child: _crearLista(),
+        ),
+      )
     );
-    
   }
-}
 
 
-  Widget _drawer(LoginBloc _bloc ){
+    Widget _drawer(LoginBloc _bloc ){
     return Drawer(
         child: ListView(
           children: [
@@ -48,11 +79,75 @@ class HomePageState extends State<HomePage> {
               ),
               Divider(thickness: 0.5),
               ListTile(
-                title: Text("Cerrar Sesión"),
+                title: Text("Salir de la app"),
+                onTap: () => Navigator.pushNamed(context, "login"),
               ),
           ],
         ));
+      }
+
+   _agregar10(){
+    for (var i=1;i<=10;i++){
+      _ultimoItem++;
+      _numeros.add(_ultimoItem);
+    }
+    setState(() {
+      
+    });
   }
+
+    Future <Null> fetchData() async {
+
+    _isLoading = true;
+    setState(() {});
+      final _duration = new Duration(seconds: 2);
+      return new Timer(_duration, respuestaHTTP);
+  }
+
+  void respuestaHTTP(){
+
+    _isLoading = false;
+
+    _agregar10();
+
+  }
+
+  _crearLista() {
+    return RefreshIndicator(
+        onRefresh: obtenerPagina1,
+        child: ListView.builder(
+        controller: _scrollController,
+        itemCount: _numeros.length,
+        itemBuilder: (BuildContext context, int index){
+
+          final imagen = _numeros[index];
+          
+          return FadeInImage(
+           placeholder: AssetImage("lib/src/resources/assets/images/25.gif"),
+           image: NetworkImage("https://picsum.photos/id/${_numeros[index]}/450/300"),
+           );
+        }),
+    );
+  }
+
+ Future<Null> obtenerPagina1() async{
+
+      final duration = new Duration( seconds:  2);
+       new Timer(duration, (){
+        _numeros.clear();
+        _ultimoItem++;
+        _agregar10();
+      });
+
+  return Future.delayed(duration);
+  }
+}
+
+
+
+
+  
+  
 
 
   
